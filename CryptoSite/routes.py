19195608e43165704676d17e678aa942df5c07b4
Andrey Russia <andrejvolodin35@gmail.com>
2025-03-24 13:44:28 +0300

    Routes.py. Исправлены ошибки

--- CryptoSite/routes.py
+++ CryptoSite/routes.py
@@ -1,7 +1,3 @@
-"""
-Routes and views for the bottle application.
-"""
-
 from bottle import route, view
 from datetime import datetime
 
@@ -11,16 +7,65 @@ from datetime import datetime
 def home():
     """Renders the home page."""
     return dict(
-        year=datetime.now().year,  # Текущий год
-        coindesk='https://www.coindesk.com',  # Сайт CoinDesk
-        coinbase_learn='https://www.coinbase.com/learn',  # Обучение от Coinbase
-        coinmarketcap='https://coinmarketcap.com',  # CoinMarketCap
-        trezor='https://www.trezor.io',  # Официальный сайт Trezor
-        bitcoin_info='/bitcoin',  # Внутренняя ссылка на страницу о Биткойне
+        year=datetime.now().year,
+        coindesk='https://www.coindesk.com',
+        coinbase_learn='https://www.coinbase.com/learn',
+        coinmarketcap='https://coinmarketcap.com',
+        trezor='https://www.trezor.io',
+        bitcoin_info='/bitcoin',
+        ethereum_org='https://ethereum.org',
+        ethereum_page='/ethereum',
+        litecoin_org='https://litecoin.org',
+        litecoin_info='/litecoin',
+        coindesk_news='https://www.coindesk.com',
+        bitcoin_how_it_works='https://www.bitcoin.org/en/how-it-works'
+
+    )
+
+@route('/ethereum')
+@view('ethereum')
+def ethereum():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year,
+        decentralization='/decentralization',
+        smart_contracts='/smart_contracts',
         ethereum_org='https://ethereum.org',  # Официальный сайт Ethereum
-        litecoin_info='/litecoin',  # Внутренняя ссылка на страницу о Лайткойне
-        coindesk_news='https://www.coindesk.com',  # Новости от CoinDesk
-        bitcoin_how_it_works='https://www.bitcoin.org/en/how-it-works'  # Как работает Биткойн
+        ethereum_what_is='https://ethereum.org/what-is-ethereum',  # Что такое Ethereum
+        ethereum_smart_contracts='https://ethereum.org/smart-contracts',  # Смарт-контракты
+        ethereum_dapps='https://ethereum.org/dapps',  # Децентрализованные приложения (DApps)
+        ethereum_how_it_works='https://ethereum.org/en/staking/#what-is-staking',  # Как работает Ethereum
+        ethereum_whitepaper='https://ethereum.org/whitepaper',  # Официальный Whitepaper
+        ethereum_github='https://github.com/ethereum',  # Исходный код на GitHub
+        ethereum_community='https://ethereum.org/community'  # Сообщество Ethereum
+    )
+
+@route('/decentralization')
+@view('decentralization')
+def decentralization():
+    """Renders the decentralization page."""
+    return dict(
+        year=datetime.now().year,  
+        decentralization_wiki='https://ru.wikipedia.org/wiki/Децентрализация',  # Ссылка на Википедию
+        decentralization_basics='https://www.investopedia.com/terms/d/decentralization.asp',  # Основы децентрализации
+        blockchain_decentralization='https://www.coindesk.com/learn/what-is-decentralization-why-is-it-important',  # Децентрализация в блокчейне
+        decentralization_benefits='https://www.forbes.com/sites/forbestechcouncil/2021/03/15/the-benefits-of-decentralization-in-blockchain',  # Преимущества децентрализации
+        decentralization_examples='https://www.blockchain-council.org/blockchain/examples-of-decentralized-systems-you-must-know',  # Примеры децентрализованных систем
+        decentralization_how_it_works='https://www.ibm.com/topics/decentralization'  # Как работает децентрализация
+    )
+
+@route('/smart_contracts')
+@view('smart_contracts')
+def smart_contracts():
+    """Renders the smart contracts page."""
+    return dict(
+        year=datetime.now().year,
+        smart_contracts_wiki='https://ru.wikipedia.org/wiki/Смарт-контракт',  # Ссылка на Википедию
+        smart_contracts_basics='https://www.investopedia.com/terms/s/smart-contracts.asp',  # Основы смарт-контрактов
+        smart_contracts_how_it_works='https://www.ibm.com/topics/smart-contracts',  # Как работают смарт-контракты
+        smart_contracts_benefits='https://www.forbes.com/sites/forbestechcouncil/2021/03/15/the-benefits-of-smart-contracts',  # Преимущества смарт-контрактов
+        smart_contracts_examples='https://www.blockchain-council.org/blockchain/smart-contract-examples',  # Примеры использования
+        smart_contracts_creation='https://ethereum.org/en/developers/docs/smart-contracts'  # Создание смарт-контрактов
     )
 
 @route('/contact')
@@ -28,16 +73,34 @@ def home():
 def contact():
     """Renders the contact page."""
     return dict(
-        title='Contact',
-        message='Your contact page.',
+        title='Контакты',
         year=datetime.now().year
     )
 
 @route('/news')
 @view('news')
 def news():
-    """Renders the about page."""
+    """Renders the news page."""
     return dict(
         title='Новости',
+        year=datetime.now().year
+    )
+
+@route('/bitcoin')
+@view('bitcoin')
+def news():
+    """Renders the bitcoin page."""
+    return dict(
+        title='История Биткойна',
+        year=datetime.now().year,
+        decentralization='/decentralization',
+    )
+
+@route('/litecoin')
+@view('litecoin')
+def news():
+    """Renders the about page."""
+    return dict(
+        title='История Лайткойна',
         year=datetime.now().year
     )
